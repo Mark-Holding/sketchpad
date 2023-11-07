9f9e67ece40119d6771fd888cdab9607a246064f
Mark H <mark-holding@hotmail.co.uk>
2023-11-07 11:16:18 +0000

    add mouseover function to chnage square color

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function createGrid(rows, columns) {
-    const gridContainer = document.getElementById('grid-container');
+const gridContainer = document.getElementById('grid-container');
 
+
+
+function createGrid(rows, columns) {
+    
     for (let i = 0; i < rows * columns; i ++){
         const square = document.createElement('div');
         square.classList.add('grid-square');
         gridContainer.appendChild(square);
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        })
     }
-
+    
+ 
 }
 
 createGrid(16, 16)
+
