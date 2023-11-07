f340fcd5c18880bd996430b6fd34ec8072fd2d81
Mark H <mark-holding@hotmail.co.uk>
2023-11-07 11:51:31 +0000

    add reset funtion and grid size selector

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const gridContainer = document.getElementById('grid-container');
+const btn = document.getElementById('btn');
 
 
 
 function createGrid(rows, columns) {
-    
+    gridContainer.innerHTML = '';
+
     for (let i = 0; i < rows * columns; i ++){
         const square = document.createElement('div');
         square.classList.add('grid-square');
@@ -18,3 +20,13 @@ function createGrid(rows, columns) {
 
 createGrid(16, 16)
 
+btn.addEventListener('click', () => {
+    let squareWidth = prompt('Enter grid width');
+    if (squareWidth <= 100) {
+    gridContainer.style.width = `${squareWidth * 50 + squareWidth * 2}px`;
+    createGrid(squareWidth, squareWidth);
+        } else {
+            alert('Number must be below 100');
+        }
+    }
+);
