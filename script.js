56363cf94138e194c055fc482f8c6b86e44f38da
Mark H <mark-holding@hotmail.co.uk>
2023-11-07 12:28:20 +0000

    add random color generator

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const gridContainer = document.getElementById('grid-container');
 const btn = document.getElementById('btn');
 
-
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i ++){
+        color += letters[Math.floor(Math.random() *16)];
+    }
+    return color;
+}
 
 function createGrid(rows, columns) {
     gridContainer.innerHTML = '';
 
     for (let i = 0; i < rows * columns; i ++){
         const square = document.createElement('div');
+
         square.classList.add('grid-square');
         gridContainer.appendChild(square);
+
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'black';
+            square.style.backgroundColor = getRandomColor();
         })
     }
     
